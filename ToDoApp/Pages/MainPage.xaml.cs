208c226e2dd7887f95c9ce5fc63aab5a29fc9d89
Salavat Abrarov <abrarov03@gmail.com>
2025-05-14 19:35:17 +0300

    Add MyTask class, added simple view for tasks

--- ToDoApp/Pages/MainPage.xaml.cs
+++ ToDoApp/Pages/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDoApp.Data;
 
 namespace ToDoApp.Pages
 {
@@ -20,14 +21,28 @@ namespace ToDoApp.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        public List<MyTask> Tasks { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            Tasks = new List<MyTask>
+            {
+                new MyTask { Name = "Task 1", Description = "Description 1" },
+                new MyTask { Name = "Task 2", Description = "Description 2" },
+                new MyTask { Name = "Task 3", Description = "Description 3" }
+            };
+            this.DataContext = this;
         }
 
         private void btnNextPage_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new EditTaskPage());
+        }
+
+        private void taskListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tbTaskDetail.Text = (taskListView.SelectedItem as MyTask)?.Description;
         }
     }
 }
